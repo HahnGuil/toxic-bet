eb17b1a181baa645475c1b4d6fc5039b6dc9218e
hahnguil <guilherme.f.h@hotmail.com>
2025-12-23 17:53:45 -0300

    feat: clean up GlobalExceptionHandler and MatchRepository by removing unused imports and methods

--- src/main/java/br/com/hahn/toxicbet/application/controller/GlobalExceptionHandler.java
+++ src/main/java/br/com/hahn/toxicbet/application/controller/GlobalExceptionHandler.java
@@ -6,13 +6,11 @@ import br.com.hahn.toxicbet.model.ErrorResponseDTO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.support.WebExchangeBindException;
 import reactor.core.publisher.Mono;
 
-import java.nio.file.AccessDeniedException;
 import java.time.Instant;
 import java.time.OffsetDateTime;
 import java.time.ZoneOffset;
--- src/main/java/br/com/hahn/toxicbet/domain/repository/MatchRepository.java
+++ src/main/java/br/com/hahn/toxicbet/domain/repository/MatchRepository.java
@@ -1,9 +1,7 @@
 package br.com.hahn.toxicbet.domain.repository;
 
 import br.com.hahn.toxicbet.domain.model.Match;
-import br.com.hahn.toxicbet.domain.model.enums.Result;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-import reactor.core.publisher.Flux;
 
 public interface MatchRepository extends ReactiveCrudRepository<Match, Long> {
 }
