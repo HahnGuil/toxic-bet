1a904a26aa078b812a6f2a751353813559ad06f7
hahnguil <guilherme.f.h@hotmail.com>
2025-12-23 17:51:43 -0300

    feat: enhance AbstractController with detailed Javadoc comments for OAuth user updates

--- src/main/java/br/com/hahn/toxicbet/application/controller/AbstractController.java
+++ src/main/java/br/com/hahn/toxicbet/application/controller/AbstractController.java
@@ -6,12 +6,27 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Mono;
 
+/**
+ * Abstract base controller providing common functionality for all controllers.
+ * Includes methods for handling OAuth user updates with logging.
+ *
+ * @author HahnGuil
+ */
 @RequiredArgsConstructor
 @Slf4j
 public abstract class AbstractController {
 
     protected final JwtService jwtService;
 
+    /**
+     * Updates the OAuth user application status with logging.
+     * Logs the process at different stages and handles errors gracefully.
+     *
+     * @param email The email of the user whose OAuth status is being updated.
+     * @return A {@link Mono} that completes when the operation is finished.
+     *
+     * @author HahnGuil
+     */
     protected Mono<Void> updateOAuthUserApplicationWithLogging(String email) {
         return jwtService.updateOAuthUserApplication()
                 .doOnSubscribe(s -> log.debug("AbstractController: Checking OAuth user status for user email:  {}, at: {}", email, DateTimeConverter.formatInstantNow()))
