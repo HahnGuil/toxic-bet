5f86b01f4bf20c9e8e5b200fdcfa41ba7b237b3d
hahnguil <guilherme.f.h@hotmail.com>
2025-12-26 18:21:55 -0300

    feat: update error messages for match validation and add new messages for match update and not found errors

--- src/main/java/br/com/hahn/toxicbet/domain/model/enums/ErrorMessages.java
+++ src/main/java/br/com/hahn/toxicbet/domain/model/enums/ErrorMessages.java
@@ -9,14 +9,16 @@ import lombok.ToString;
 @ToString
 public enum ErrorMessages {
 
-    INVALID_MATCH_TIME("The departure date has already passed."),
+    INVALID_MATCH_TIME("The date of the match has already passed, please provide a future date."),
     HOME_TEAM_NOT_FOUND("Home Team not found, or not registered."),
     VISITING_TEAM_NOT_FOUND("Visiting Team not found, or not registered."),
     INVALID_DATE_MATCH_TIME("Invalid match date format. Match date must be DD/MM/YYYY HH:MM"),
     GENERIC_INVALID_FORMAT("There is an invalid format in the request; please check the fields entered and try again."),
     UNAUTHORIZED_MESSAGE("Unauthorized user. Please log in or verify the access credentials for this resource."),
     TEAM_MUST_BE_DIFFERENT("The home team and the visiting team cannot be the same."),
-    CONFLICT_MATCH_TIME("One of the teams in the match already has a game scheduled for this date and time.");
+    CONFLICT_MATCH_TIME("One of the teams in the match already has a game scheduled for this date and time."),
+    CAN_NOT_UPDATE_MATCH("Cannot update score for match not in progress"),
+    MATCH_NOT_FOUND("Match not found for this id: ");
 
     private final String message;
 }
