0e46b659c52035dd83ca066a865010bae58793a8
hahnguil <guilherme.f.h@hotmail.com>
2026-01-19 23:37:49 -0300

    refactor: remove unused UserNotFoundException import from UserService

--- src/main/java/br/com/hahn/toxicbet/application/service/UserService.java
+++ src/main/java/br/com/hahn/toxicbet/application/service/UserService.java
@@ -1,7 +1,6 @@
 package br.com.hahn.toxicbet.application.service;
 
 import br.com.hahn.toxicbet.application.mapper.UserMapper;
-import br.com.hahn.toxicbet.domain.exception.UserNotFoundException;
 import br.com.hahn.toxicbet.domain.model.Users;
 import br.com.hahn.toxicbet.domain.model.enums.ErrorMessages;
 import br.com.hahn.toxicbet.domain.repository.UserRepository;
