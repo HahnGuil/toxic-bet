5e5d2b3c20bae18d0563c98a3df7fd5a913ddac8
hahnguil <guilherme.f.h@hotmail.com>
2026-04-28 17:15:57 -0300

    feat: update CORS configuration to allow localhost for development

--- src/main/java/br/com/hahn/toxicbet/infrastructure/config/CorsConfig.java
+++ src/main/java/br/com/hahn/toxicbet/infrastructure/config/CorsConfig.java
@@ -10,7 +10,8 @@ public class CorsConfig implements WebFluxConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://toxicbet.com.br")
+                .allowedOrigins("https://toxicbet.com.br",
+                        "http://localhost:4200")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowCredentials(true);
     }
